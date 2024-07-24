0642c0a134c0c152db9c0ccfd6f64c6df0799ce7
musjawai <mustafajawaid@berkeley.edu>
2024-07-24 15:33:50 -0700

    added db

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const connectDB = require("./config/dbConn");
 
 const express = require("express");
 app = express();
 
+connectDB();
+
 app.use("/register", require("./routes/register"));
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
